a371e320c7789531d6e9417c5ee2c893913a71b1
khankhatarnak123 <khankhatarnak1998@gmail.com>
2025-03-19 12:48:12 +0530

    Fix Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Shopping-App/",
+  // base: "/Shopping-App/", // for github
+  base: process.env.VERCEL ? "/" : "/Shopping-App/", // 👈 Fix for Vercel & GitHub Pages
 });
